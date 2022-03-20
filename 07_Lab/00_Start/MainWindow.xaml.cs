14144bbcc3ff24c230764774875cac8f2030b2d7
StupidAI <minenkoea@gmail.com>
2022-03-20 19:14:38 +0500

    Lab 7

---
+++ 07_Lab/00_Start/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _00_Start
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Заглушка: Открытие документа");
+        }
+
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Заглушка: Сохранение документа");
+        }
+
+        // Определяем условие когда команда может быть доступна в XAML разметке к Command добавляется CanExecute
+        private void ExitCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (txtBox!=null && txtBox.Text.Length == 0) // Как только строка перестанет быть пустой нажать на выход не выйдет
+            {
+                e.CanExecute = true;
+            }
+            else
+            {
+                e.CanExecute = false;
+            }
+        }
+    }
+}
---
+++ 07_Lab/00_Start/MyCommands.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace _00_Start
+{
+    class MyCommands
+    {
+        /* Класс который будет содержать все команды нашего приложения
+         * Команды это свойства класса но они должны быть типа RoutedCommand, RoutedUICommand (using System.Windows.Input;)
+         * RoutedCommand, RoutedUICommand - не содердат логики выполнения команды как таковой, суть в том что бы генерировать событие
+         * которое начнет по дереву искать CommandBinding
+         * 
+         * Источниками команды могут быть только те элементы которые реализуют интерфейс ICommandSource
+         */
+
+
+        //// Свойства
+        //public static RoutedCommand Exit { get; set; }
+
+        //// Статический конструктор класса для инициализации команды
+        //static MyCommands()
+        //{
+        //    //Exit = new RoutedCommand(); // Этого достаточно что бы использовать команду.
+        //    InputGestureCollection inp = new InputGestureCollection(); // набор команд
+        //    inp.Add(new KeyGesture(Key.T, ModifierKeys.Control, "Ctrl+T")); //добавление к коллекции набора команд. Есть еще MouseGesture. Менюитем потом сам возьмет этот текст и добавит его в интерфейс
+        //    Exit = new RoutedCommand("Exit", typeof(MyCommands), inp); //3й конструктор
+        //}
+
+        // Все то же только с RoutedUICommand
+        // Свойства
+        public static RoutedUICommand Exit { get; set; }
+
+        // Статический конструктор класса для инициализации команды
+        static MyCommands()
+        {
+            //Exit = new RoutedCommand(); // Этого достаточно что бы использовать команду.
+            InputGestureCollection inp = new InputGestureCollection(); // набор команд
+            inp.Add(new KeyGesture(Key.T, ModifierKeys.Control, "Ctrl+T")); //добавление к коллекции набора команд. Есть еще MouseGesture. Менюитем потом сам возьмет этот текст и добавит его в интерфейс
+            Exit = new RoutedUICommand("Выход", "Exit", typeof(MyCommands), inp); //По сравнению с RoutedCommand в XAML разметке не надо будет писать Header. Только Header можно так просто сделать. В кнопке такое сделать не так просто
+        }
+
+    }
+}
