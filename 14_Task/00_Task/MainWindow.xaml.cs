6324a08b35227f4709d9de7d93bc3094551c64de
StupidAI <minenkoea@gmail.com>
2022-03-25 18:14:33 +0500

    Task 14 review

---
+++ 14_Task/00_Task/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _00_Task
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products; // using System.Collections.ObjectModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            products = new ObservableCollection<Product>(); //Данный тип позволить получать уведомления об изменениях списка
+            // 1 продукт
+            products.Add(new Product()
+            {
+                ProductName = "Холодильник",
+                Price = 200,
+                ImagePath = "data/h.jpg",
+                Category = Category.Tech
+            });
+            // 2 продукт
+            products.Add(new Product()
+            {
+                ProductName = "Лапша",
+                Price = 5,
+                ImagePath = "data/l.jpg",
+                Category = Category.Food
+            });
+            // 3 продукт
+            products.Add(new Product()
+            {
+                ProductName = "Микроволновка",
+                Price = 75,
+                ImagePath = "data/m.jpg",
+                Category = Category.Tech
+            });
+            // 4 продукт
+            products.Add(new Product()
+            {
+                ProductName = "Сахар",
+                Price = 1000000,
+                ImagePath = "data/s.jpg",
+                Category = Category.Food
+            });
+            lstBox.ItemsSource = products;
+        }
+    }
+}
---
+++ 14_Task/00_Task/Product .cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_Task
+{
+    /* Определить класс Product для хранения информации о товаре. Класс должен содержать характеристики 
+     * Название товара (строка), 
+     * Цена (число), 
+     * Изображение (строка - путь к файлу с изображением), 
+     * Категория (перечисление, возможные значения – Еда, Бытовая техника)
+     */
+
+    // Список категорий
+    public enum Category
+    {
+        Food = 1,
+        Tech = 2
+    }
+
+    // Класс продукта
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public string ImagePath { get; set; }
+        public Category Category { get; set; }
+
+        // переопределение метода для тестирования вывода информации о продукте в строку
+        public override string ToString()
+        {
+            return $"{ProductName}: {Price} - {Category}";
+        }
+    }
+}
