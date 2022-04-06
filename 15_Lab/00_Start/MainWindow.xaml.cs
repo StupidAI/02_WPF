5504c77263d3ee28d52c7e85e0385dd694bafdaa
StupidAI <minenkoea@gmail.com>
2022-04-06 17:06:56 +0500

    Lab15 + Task15

---
+++ 15_Lab/00_Start/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Xps;
+using System.Windows.Xps.Packaging;
+
+namespace _00_Start
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_OpenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            XpsDocument doc = new XpsDocument("data/2.xps", FileAccess.Read); // using System.Windows.Xps.Packaging; (... using System.IO;)
+            docViewer.Document = doc.GetFixedDocumentSequence();
+            doc.Close();
+        }
+
+        private void btn_SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            XpsDocument doc = new XpsDocument("1.xps", FileAccess.Write); // using System.Windows.Xps.Packaging; (... using System.IO;)
+            XpsDocumentWriter writer = XpsDocument.CreateXpsDocumentWriter(doc); // using System.Windows.Xps;
+            writer.Write(docViewer.Document as FixedDocument);
+            doc.Close();
+        }
+    }
+}
---
+++ 15_Lab/02_FlowDoc_Practice/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace _02_FlowDoc_Practice
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Clear_Click(object sender, RoutedEventArgs e)
+        {
+            docViewer.ClearValue(FlowDocumentScrollViewer.DocumentProperty);
+        }
+
+        private void btn_Save_Click(object sender, RoutedEventArgs e)
+        {
+            using(FileStream fs = File.Open("1.xaml", FileMode.Create))
+            {
+                XamlWriter.Save(docViewer.Document, fs); // using System.Windows.Markup;
+            }
+        }
+
+        private void btn_Open_Click(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = File.Open("1.xaml", FileMode.Open))
+            {
+                docViewer.Document = XamlReader.Load(fs) as FlowDocument;
+            }
+        }
+
+        private void btn_Like_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Мне тоже не нравится дизайн. Завтра сделаю лучше!", "Важное дополнение", MessageBoxButton.OK);
+        }
+    }
+}
